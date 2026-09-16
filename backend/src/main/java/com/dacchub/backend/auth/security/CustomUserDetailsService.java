35f0c7a8519575a2244c764ce24626ae07705430
cauan745 <cauanramos745@gmail.com>
2026-09-16 16:26:06 -0400

    use email as username in UserDetails

--- backend/src/main/java/com/dacchub/backend/auth/security/CustomUserDetailsService.java
+++ backend/src/main/java/com/dacchub/backend/auth/security/CustomUserDetailsService.java
@@ -28,7 +28,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     return new org.springframework.security.core.userdetails.User(
-        user.getName(),
+        user.getEmail(),
         user.getPassword(),
         Collections.emptyList());
   }
